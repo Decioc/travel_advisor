06c743c5517d0a5b1fe86a7ffa5f93c42fa7b172
decioc <decioc.kyndryl.com>
2025-04-01 11:19:21 -0400

    json console

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,3 +2,42 @@ function book() {
     alert("Congrats!! You booked your travel!!")
 }
 
+    function showConsole() {
+
+        fetch('travel_recommendation_api.json')
+          .then(response => response.json())
+          .then(data => {
+            const countries = data.countries;
+            const temples = data.temples;
+            const beaches = data.beaches;
+
+            countries.forEach(element => {
+                console.log(element.name);
+                const cities = element.cities;
+                cities.forEach(element => {
+                    console.log(element.name);
+                    console.log(element.imageUrl);
+                    console.log(element.description);
+                } )
+                
+            });
+
+            temples.forEach(element => {
+                console.log(element.name);
+                console.log(element.imageUrl);
+                console.log(element.description);
+            })
+
+
+            beaches.forEach(element => {
+                console.log(element.name);
+                console.log(element.imageUrl);
+                console.log(element.description);
+            })
+            
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            /* resultDiv.innerHTML = 'An error occurred while fetching data.'; */
+          });
+    }
